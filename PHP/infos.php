225d322c26a300d48800f16d70ccb842a49053ac
juvedibuar00 <130672236+juvedibuar00@users.noreply.github.com>
2023-11-18 10:09:40 -0300

    Create infos.php

---
+++ PHP/infos.php
@@ -0,0 +1,8 @@
+<?php
+//Configuração do banco de dados. Acesso ao arquivo infos.php, insira os dados de RDS, Endpoint, 
+//username, password e o nome banco de dados
+$host = "juvedb.c6dbsec3x5u6.sa-east-1.rds.amazonaws.com";
+$username = "dibuar";
+$password = "wG9W7tpD8Ebt4vLeezSe";
+$database = "JUVEdb";
+?>
